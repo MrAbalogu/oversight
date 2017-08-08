bcad961b6597ea1bd59aba1b0a03e8dea3730038
mrabalogu <chineduabalogu@yahoo.com>
2017-08-08 10:29:47 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/oversight">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
